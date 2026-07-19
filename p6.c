b7b8d625b34463719d3ee42d384a4b0bf99ff468
Chethu08 <113187431+Chethu08@users.noreply.github.com>
2026-07-20 03:53:15 +0530

    Update p6.c

--- p6.c
+++ p6.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
-int main(){
-    int w[10],p[10],k[10][10],n,m,i,j;
-    printf("Enter objects: "); scanf("%d",&n);
+int main() {
+    int w[10], p[10], k[10][10], n, m, i, j;
+    printf("Enter objects: ");
+    scanf("%d", &n);
     printf("Enter weights:\n");
-    for(i=1;i<=n;i++) scanf("%d",&w[i]);
+    for(i=1; i<=n; i++)
+        scanf("%d", &w[i]);
     printf("Enter profits:\n");
-    for(i=1;i<=n;i++) scanf("%d",&p[i]);
-    printf("Enter capacity: "); scanf("%d",&m);
-    for(i=0;i<=n;i++)
-        for(j=0;j<=m;j++)
-            if(!i||!j) k[i][j]=0;
-            else if(w[i]<=j)
-                k[i][j]=(p[i]+k[i-1][j-w[i]]>k[i-1][j])?p[i]+k[i-1][j-w[i]]:k[i-1][j];
-            else k[i][j]=k[i-1][j];
-    printf("Optimal solution=%d",k[n][m]);
+    for(i=1; i<=n; i++)
+        scanf("%d", &p[i]);
+    printf("Enter capacity: ");
+    scanf("%d", &m);
+    for(i=0; i<=n; i++)
+        for(j=0; j<=m; j++)
+            if(i==0 || j==0)
+                k[i][j]=0;
+            else if(w[i]<=j){
+                if(p[i]+k[i-1][j-w[i]] > k[i-1][j])
+                    k[i][j]=p[i]+k[i-1][j-w[i]];
+                else
+                    k[i][j]=k[i-1][j];
+            }
+            else
+                k[i][j]=k[i-1][j];
+    printf("Optimal solution=%d", k[n][m]);
 }
